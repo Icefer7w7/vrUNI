e474ea8588ee4e7d41fec44e216ef40c0e968d75
Icefer7w7 <miguelangelgarzonabu@gmail.com>
2025-11-11 21:36:58 -0500

    Refactor main.js for improved structure and performanceer

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ const controls = new OrbitControls( camera, renderer.domElement );
 let gamepad;
 let moveForward = false;
 let moveBackward = false;
-const speed = 0.1;
+const speed = 0.3;
 const gravity = 0.01;
 
 const character = new THREE.Object3D();
@@ -527,3 +527,4 @@ function animate() {
 
   renderer.render(scene, camera);
 }
+
